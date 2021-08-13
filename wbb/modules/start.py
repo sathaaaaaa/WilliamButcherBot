1e38a6968de371ed3a2b5402f21fa329204f94b1
sathaaaaaa <85862925+sathaaaaaa@users.noreply.github.com>
2021-08-13 11:51:38 +0530

    Add files via upload

---
+++ wbb/modules/start.py
@@ -0,0 +1,6 @@
+from wbb import app
+from pyrogram import filters
+
+@app.on_message(filters.command("start"))
+ async def start_command(_, message):
+     await message.reply_text("Hi I'm Yumiko and I'm already up")
